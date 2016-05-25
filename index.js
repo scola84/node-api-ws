44591871ccb4a1db416ecebb55235d02322696f2
M.C. van den Boogaart <scola@kvdb.name>
2016-05-25 10:59:43 +0200

    Improve event handling

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
-  http: require('@scola/api-http'),
   Connection: require('./lib/connection'),
-  Connector: require('./lib/connector'),
-  WebSocket: require('./lib/websocket-wrapper')
+  Connector: require('./lib/connector')
 };
