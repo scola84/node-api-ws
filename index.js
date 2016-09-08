de45c22db97f2f4625973e75dc0df615e2bc7014
M.C. van den Boogaart <scola@kvdb.name>
2016-09-08 22:52:59 +0200

    Remove obsolete exports

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-export { default as ClientRequest } from './src/client-request';
-export { default as ClientResponse } from './src/client-response';
 export { default as Connection } from './src/connection';
 export { default as Connector} from './src/connector';
 export { default as data } from './src/i18n/data';
