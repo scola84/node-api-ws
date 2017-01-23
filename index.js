d6eb437d90fc5925f0704d83026ae048a4cbc851
M.C. van den Boogaart <scola@kvdb.name>
2017-01-23 17:13:01 +0100

    Load on app

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import strings from './src/i18n/strings';
 export { default as WsConnection } from './src/connection';
 export { default as WsConnector } from './src/connector';
 
-export function load(i18n) {
-  i18n.strings(strings);
+export function load(app) {
+  app.i18n().strings(strings);
 }
