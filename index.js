77efc0cf06767b238fabb1352e3ce9bfe6a9c436
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:26:41 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import WsConnection from './src/connection';
+import WsConnector from './src/connector';
 import strings from './src/i18n/strings';
 
-export { default as WsConnection } from './src/connection';
-export { default as WsConnector } from './src/connector';
-
-export function load(app) {
+function load(app) {
   if (app.i18n()) {
     app.i18n().strings(strings);
   }
 }
+
+export {
+  WsConnection,
+  WsConnector,
+  load
+};
