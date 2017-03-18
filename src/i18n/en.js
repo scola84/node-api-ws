ca1bed609d64a64b0f42d02e70f334b7f73d2b10
M.C. van den Boogaart <scola@kvdb.name>
2017-03-18 17:23:09 +0100

    Update error messages

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,15 +1,21 @@
 export default {
   scola: {
     error: {
+      invalid_data: 'The data was not found.',
       invalid_protocol: 'A protocol error occurred.',
+      invalid_query: 'A server error occurred.',
       invalid_request: 'A message error occurred.',
       invalid_response: 'A message error occurred.',
-      invalid_socket: 'A socket error occurred.',
+      invalid_socket: 'A connection error occurred.',
+      invalid_state: 'Your request is being processed.',
       short: {
+        invalid_data: 'No Data',
         invalid_protocol: 'Protocol Error',
+        invalid_query: 'Server Error',
         invalid_request: 'Message Error',
         invalid_response: 'Message Error',
         invalid_socket: 'Connection Error',
+        invalid_state: 'Processing Error'
       }
     }
   }
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,15 +1,21 @@
 export default {
   scola: {
     error: {
+      invalid_data: 'De gegevens zijn niet gevonden.',
       invalid_protocol: 'Er is een protocolfout opgetreden.',
+      invalid_query: 'Er is een serverfout opgetreden.',
       invalid_request: 'Er is een berichtfout opgetreden.',
       invalid_response: 'Er is een berichtfout opgetreden.',
       invalid_socket: 'Er is een verbindingsfout opgetreden.',
+      invalid_state: 'Uw aanvraag wordt verwerkt.',
       short: {
+        invalid_data: 'Geen gegevens',
         invalid_protocol: 'Protocolfout',
+        invalid_query: 'Serverfout',
         invalid_request: 'Berichtfout',
         invalid_response: 'Berichtfout',
         invalid_socket: 'Verbindingsfout',
+        invalid_state: 'Verwerkingsfout'
       }
     }
   }
